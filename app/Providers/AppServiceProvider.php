285bf0219b5d3fd4d30708bf5138a43f5d01ba90
Takumi-JOMO <t.shimada@jomo-inc.co.jp>
2020-07-23 08:24:51 +0900

    modified https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
-        $url->forceSchema('https');
+        $url->forceScheme('https');
         Schema::defaultStringLength(191);
     }
 }
